0e7db8b992d3ceb27e87eb0246fcbffc946930a6
kdfarquharson <52825009+kdfarquharson@users.noreply.github.com>
2022-06-30 17:38:24 +0100

    Update OverWriteDrive.ps1

--- OverWriteDrive.ps1
+++ OverWriteDrive.ps1
@@ -2,8 +2,8 @@
 # This won't erase existing content
 # Text Iain M. Banks(c) - The Algebraist
 # Probably not the fastest, or most secure, but it'll do for simplicity
-# TO DO:
-# It will run continuously then throw an error, but keep running - Catch this error and cease
+
+
 $outputString = "I was born in a water moon. Some people, especially its inhabitants, called it a planet, but as it was only a little over two hundred kilometres in diameter, 'moon' seems the more accurate term. The moon was made entirely of water, by which I mean it was a globe that not only had no land, but no rock either, a sphere with no solid core at all, just liquid water, all the way down to the very centre of the globe.
 If it had been much bigger the moon would have had a core of ice, for water, though supposedly incompressible, is not entirely so, and will change under extremes of pressure to become ice. (If you are used to living on a planet where ice floats on the surface of water, this seems odd and even wrong, but nevertheless it is the case.) The moon was not quite of a size for an ice core to form, and therefore one could, if one was sufficiently hardy, and adequately proof against the water pressure, make one's way down, through the increasing weight of water above, to the very centre of the moon.
 Where a strange thing happened.
@@ -30,7 +30,14 @@ New-Item -ItemType Directory -Force -Path "$($outputDrive)$($hexFolder)"
 
 do {
     $hexFile = $iFile.ToString("X")
-    $outputString | Out-File -FilePath "$($outputDrive)$($hexFolder)\$($hexFile).txt" -Force
+    try{
+        $outputString | Out-File -FilePath "$($outputDrive)$($hexFolder)\$($hexFile).txt" -Force
+    }
+    catch{
+        Write-Host "Message: $($_.Exception)"
+        Write-Host "Check the drive $($outputDrive) is full"
+        break
+    }
     $iFile = $iFile + 1
     
     if ($iFile -gt $filePerDir){
